e92746ceaa32119e106e98644c8c045af6290329
Quas <aaron.hickman201@gmail.com>
2023-06-12 14:51:00 +0100

    added comment model

--- models/comments.js
+++ models/comments.js
@@ -1,5 +1,30 @@
 const { Schema, model } = require("mongoose");
 const moment = require("moment"); 
 
+const commentSchema = new Schema = (
+    {
+        comment: {
+            type: String,
+            required: true,
+            minlength: 1
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        created: {
+            type: Date,
+            default: Date.now,
+            get: formatDate
+        },
+        toJSON: {
+            getters: true,
+        },
+    })
+
+    const Comment = model("comment", commentSchema);
+
+    module.exports = Comment; 
+
 
 
